508cc09c7b0799d79a253b70451d450ba04ee98e
Nail Sharipov <nailxsharipov@gmail.com>
2020-03-03 18:00:25 +0300

    add util extension

--- Runtime/iShape/Triangulation/Shape/TriangulationStrategy.cs
+++ Runtime/iShape/Triangulation/Shape/TriangulationStrategy.cs
@@ -4,5 +4,9 @@ namespace iShape.Triangulation.Shape {
         delaunay = 0,
         plain = 1
     }
+    
+    public static class TriangulationStrategyExtension {
+        public static string[] Enums = {TriangulationStrategy.delaunay.ToString(), TriangulationStrategy.plain.ToString()};
+    }
 
 }
